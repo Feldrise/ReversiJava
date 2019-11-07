3b5439568afc849ef918fbc6ab1870e624f96cea
Feldrise <admin@feldrise.com>
2019-11-07 15:44:35 +0100

    [Create] Globale variables

---
+++ src/reversi/jeu.java
@@ -0,0 +1,43 @@
+package reversi;
+
+public class jeu {
+	
+	//variables globales
+	public static int joueur; // Correspond au numéro du joueur jouant le tour
+	public static int taille; // Correspond à la taille du plateau (qui est un carré de taille x taille)
+	public static int[][] plateau; // Contient les pions
+	public static boolean passe; // Indique si le joueur précédent à passé son tour.
+
+	/************************ Partie 1 ************************/
+	/**
+	 * Cette fonction initialise le jeu
+	 * @param taille
+	 * @param regle
+	 */
+	public static void init(int taille, boolean regle)  {
+		
+	}
+	
+
+
+	/************************ Partie 2 ************************/
+	
+
+
+
+	/************************ Partie 3 ************************/
+	
+
+
+	
+	/************************ Partie 4 ************************/
+	
+
+
+	
+	/*************************** main ***************************/
+	public static void main(String[] args) {
+	
+	}
+
+}
